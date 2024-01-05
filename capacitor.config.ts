a75e4c7d320b6d141a9d6d82f3611b0582589f11
bucky-badger-gesmer <aaron.gesmer@gmail.com>
2024-01-05 12:02:51 -0600

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'watches-exercise-ii',
+  webDir: 'dist',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
