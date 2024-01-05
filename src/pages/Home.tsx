d0f443d11165c00fc6928b5e84d11e2989c9498a
bucky-badger-gesmer <aaron.gesmer@gmail.com>
2024-01-05 15:49:28 -0600

    feat: can swipe through components

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -16,11 +16,31 @@ import {
   IonToolbar,
 } from "@ionic/react";
 import { chevronBack, close } from "ionicons/icons";
+import { Swiper, SwiperSlide } from "swiper/react";
+
+import "@ionic/react/css/ionic-swiper.css";
+import { useState } from "react";
+import "swiper/css";
+import CreateListing from "./CreateListing";
 import "./Home.css";
 
 const Home: React.FC = () => {
+  const [prevSlideIndex, setPrevSlideIndex] = useState(0);
+
+  const handleSlideChange = (swiper: any) => {
+    const currentSlideIndex = swiper.realIndex;
+
+    if (currentSlideIndex > prevSlideIndex) {
+      console.log("Swiped Right", currentSlideIndex, prevSlideIndex);
+    } else if (currentSlideIndex < prevSlideIndex) {
+      console.log("Swiped Left", currentSlideIndex, prevSlideIndex);
+    }
+
+    setPrevSlideIndex(currentSlideIndex);
+  };
+
   return (
-    <IonPage>
+    <IonPage style={{ justifyContent: "center", alignItems: "center" }}>
       <IonHeader>
         <IonToolbar>
           <IonButtons slot="start">
@@ -36,7 +56,7 @@ const Home: React.FC = () => {
           </IonButtons>
         </IonToolbar>
       </IonHeader>
-      <IonContent fullscreen>
+      <IonContent fullscreen style={{ maxWidth: "740px" }}>
         <IonCard>
           <IonCardContent>
             <IonGrid>
@@ -59,6 +79,44 @@ const Home: React.FC = () => {
             </IonGrid>
           </IonCardContent>
         </IonCard>
+        <ul className="tabs">
+          <li
+            style={{
+              textDecoration: prevSlideIndex === 0 ? "underline" : "none",
+            }}
+          >
+            DETAILS
+          </li>
+          <li
+            style={{
+              textDecoration: prevSlideIndex === 1 ? "underline" : "none",
+            }}
+          >
+            PHOTOS
+          </li>
+          <li
+            style={{
+              textDecoration: prevSlideIndex === 2 ? "underline" : "none",
+            }}
+          >
+            PRICE
+          </li>
+        </ul>
+        <Swiper
+          slidesPerView={1}
+          spaceBetween={20}
+          onSlideChange={handleSlideChange}
+        >
+          <SwiperSlide>
+            <CreateListing />
+          </SwiperSlide>
+          <SwiperSlide>
+            <IonCard style={{ width: "100%", height: 500 }}>poop2</IonCard>
+          </SwiperSlide>
+          <SwiperSlide>
+            <IonCard style={{ width: "100%", height: 500 }}>poop3</IonCard>
+          </SwiperSlide>
+        </Swiper>
       </IonContent>
       <IonFooter>
         <IonToolbar>
