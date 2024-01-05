696f81aa3d00a72300b4dcaebfc2869a052fdd87
bucky-badger-gesmer <aaron.gesmer@gmail.com>
2024-01-05 14:27:04 -0600

    feat: home page setup

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,23 +1,45 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
-import ExploreContainer from '../components/ExploreContainer';
-import './Home.css';
+import {
+  IonButton,
+  IonButtons,
+  IonContent,
+  IonFooter,
+  IonHeader,
+  IonIcon,
+  IonPage,
+  IonTitle,
+  IonToolbar,
+} from "@ionic/react";
+import { chevronBack, close } from "ionicons/icons";
+import "./Home.css";
 
 const Home: React.FC = () => {
   return (
     <IonPage>
       <IonHeader>
         <IonToolbar>
-          <IonTitle>Blank</IonTitle>
+          <IonButtons slot="start">
+            <IonButton>
+              <IonIcon slot="icon-only" icon={chevronBack} />
+            </IonButton>
+          </IonButtons>
+          <IonTitle>CREATE LISTING</IonTitle>
+          <IonButtons slot="end">
+            <IonButton>
+              <IonIcon slot="icon-only" icon={close} />
+            </IonButton>
+          </IonButtons>
         </IonToolbar>
       </IonHeader>
-      <IonContent fullscreen>
-        <IonHeader collapse="condense">
-          <IonToolbar>
-            <IonTitle size="large">Blank</IonTitle>
-          </IonToolbar>
-        </IonHeader>
-        <ExploreContainer />
+      <IonContent fullscreen className="ion-padding">
+        <h1>Content</h1>
       </IonContent>
+      <IonFooter>
+        <IonToolbar>
+          <IonButton color="light" style={{ width: "100%" }}>
+            Next
+          </IonButton>
+        </IonToolbar>
+      </IonFooter>
     </IonPage>
   );
 };
